e7c0f20767e40d829ea7c37b329b27ee171b9189
BunnyBlack <xyy6836@126.com>
2021-01-27 11:07:03 +0800

    Rearrange Player.cs script

---
+++ Assets/Scripts/Core/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Core.Player
+{
+    public class Player : MonoBehaviour
+    {
+        private void Start()
+        {
+            InitPosition();
+        }
+
+        private void InitPosition()
+        {
+            gameObject.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+}
