78052e42d13b45d6a0e80cf5cffe01eba746f08e
BunnyBlack <xyy6836@126.com>
2021-01-29 17:01:25 +0800

    Enemy movement

---
+++ Assets/Scripts/Core/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Core.Enemy
+{
+    public class Enemy : MonoBehaviour
+    {
+        private float _speed = 4f;
+
+        private const float BottomBoarder = -5.5f;
+        private const float TopBoarder = 7f;
+        private const float LeftBoarder = -9f;
+        private const float RightBoarder = 9f;
+
+        private void Update()
+        {
+            MoveDown();
+
+            if (gameObject.transform.position.y < BottomBoarder)
+            {
+                ReSpawn();
+            }
+        }
+
+        private void ReSpawn()
+        {
+            var respawnPositionX = Random.Range(LeftBoarder, RightBoarder);
+            var curPosition = gameObject.transform.position;
+            gameObject.transform.position = new Vector3(respawnPositionX, TopBoarder, curPosition.z);
+        }
+
+        private void MoveDown()
+        {
+            gameObject.transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+        }
+    }
+}
