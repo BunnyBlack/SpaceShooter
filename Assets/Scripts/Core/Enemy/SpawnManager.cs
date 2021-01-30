afb0bce765b710df867283aa66e95c13e2f5e6e1
BunnyBlack <xyy6836@126.com>
2021-01-30 10:07:30 +0800

    Create spawn manager

---
+++ Assets/Scripts/Core/Enemy/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Core.Enemy
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _enemyPrefeb;
+        [SerializeField] private int _maxNum = 5;
+
+        private int _curNum;
+
+        private void Start()
+        {
+            _curNum = 0;
+            StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(5);
+
+                if (_enemyPrefeb == null || _curNum >= _maxNum)
+                    continue;
+
+                var enemy = Instantiate(_enemyPrefeb);
+                enemy.GetComponent<Enemy>()?.ReSpawn();
+                _curNum++;
+            }
+        }
+    }
+}
