2a4da6c475e82d09faaab33a50903c5edac5e322
BunnyBlack <xyy6836@126.com>
2021-02-07 14:47:09 +0800

    Create asstroid object

---
+++ Assets/Scripts/Core/Enemy/Astroid.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core.Enemy
+{
+    public class Astroid : MonoBehaviour
+    {
+        [SerializeField] private float _rotateSpeed = 19.0f;
+
+        private void Update()
+        {
+            gameObject.transform.Rotate(0, 0, _rotateSpeed * Time.deltaTime);
+        }
+    }
+}
