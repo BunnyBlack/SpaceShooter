2de8fe65f23ef95ae6292b8d9562d18c9a4a7084
BunnyBlack <xyy6836@126.com>
2021-02-07 16:16:26 +0800

    Start spawn when astroid is destroyed

--- Assets/Scripts/Core/Enemy/Astroid.cs
+++ Assets/Scripts/Core/Enemy/Astroid.cs
@@ -7,7 +7,14 @@ namespace Core.Enemy
     {
         [SerializeField] private float _rotateSpeed = 19.0f;
         [SerializeField] private GameObject _explosionObj;
-        
+
+        private SpawnManager _spawnManager;
+
+        private void Start()
+        {
+            _spawnManager = GameObject.Find("/SpawnManager")?.GetComponent<SpawnManager>();
+        }
+
         private void Update()
         {
             gameObject.transform.Rotate(0, 0, _rotateSpeed * Time.deltaTime);
@@ -15,13 +22,14 @@ namespace Core.Enemy
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag("Laser"))
-            {
-                _rotateSpeed = 0;
-                Instantiate(_explosionObj, gameObject.transform.position, Quaternion.identity);
-                Destroy(other.gameObject);
-                Destroy(gameObject, 0.25f);
-            }
+            if (!other.CompareTag("Laser"))
+                return;
+
+            _rotateSpeed = 0;
+            Instantiate(_explosionObj, gameObject.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            _spawnManager.StartSpawning();
+            Destroy(gameObject, 0.25f);
         }
     }
 }
